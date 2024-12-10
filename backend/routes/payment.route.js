4609362c80ab6539242ab3493d94f237db24c934
MafiieuHell <mehdikerkar@outlook.fr>
2024-12-10 12:13:01 +0100

    paymentRoutes

---
+++ backend/routes/payment.route.js
@@ -0,0 +1,13 @@
+import express from "express";
+import { protectRoute } from "../middleware/auth.middleware.js";
+import {
+  createCheckoutSession,
+  checkoutSessionSuccess,
+} from "../controllers/payment.controller.js";
+
+const router = express.Router();
+
+router.post("/create-checkout-session", protectRoute, createCheckoutSession);
+router.post("/checkout-success", protectRoute, checkoutSessionSuccess);
+
+export default router;
