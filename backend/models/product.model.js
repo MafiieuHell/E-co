73bee67401fcd009f346751b67c87af6814d6781
MafiieuHell <mehdikerkar@outlook.fr>
2024-12-10 10:11:10 +0100

    cartRoutes

--- backend/models/product.model.js
+++ backend/models/product.model.js
@@ -1,33 +1,36 @@
 import mongoose from "mongoose";
 
-const productSchema = new mongoose.Schema({
-    naeme: {
-        type: String,
-        required: [true, "Please add a name"],  
+const productSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please add a name"],
     },
     description: {
-        type: String,
-        required: [true, "Please add a description"],
+      type: String,
+      required: [true, "Please add a description"],
     },
     price: {
-        type: Number,
-        min: [0, "Price must be at least 0"],
-        required: [true, "Please add a price"],
+      type: Number,
+      min: [0, "Price must be at least 0"],
+      required: [true, "Please add a price"],
     },
     image: {
-        type: String,
-        required: [true, "Please add an image"],
+      type: String,
+      required: [true, "Please add an image"],
     },
     category: {
-        type: String,
-        required: [true, "Please add a category"],
+      type: String,
+      required: [true, "Please add a category"],
     },
     isFeatured: {
       type: Boolean,
-      default: false  
-    }
-}, { timestamps: true });
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
 
 const Product = mongoose.model("Product", productSchema);
 
-export default Product
+export default Product;
