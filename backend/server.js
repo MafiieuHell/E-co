bb1577c6367e7e40e2ca36850dbd74fec499547c
MafiieuHell <mehdikerkar@outlook.fr>
2024-12-19 16:51:09 +0100

    try

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// const __dirname = path.resolve();
+const __dirname = path.resolve();
 
 app.use(express.json({ limit: "10mb" })); // allows you to parse the body of the request
 app.use(cookieParser());
